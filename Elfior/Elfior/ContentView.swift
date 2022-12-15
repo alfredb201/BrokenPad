8e51f219c7ae3dc911f7aceff698b5b782ab2090
Kynigoi <113687885+Kynigoi@users.noreply.github.com>
2022-12-15 13:00:48 +0100

    Game logo

--- Elfior/Elfior/ContentView.swift
+++ Elfior/Elfior/ContentView.swift
@@ -12,11 +12,7 @@ struct ContentView: View {
     var scene = SKScene(fileNamed: "Scene")!
     
     var body: some View {
-        SpriteView(scene: self.scene, debugOptions: [
-            .showsFPS,
-            .showsPhysics,
-            .showsDrawCount
-        ])
+        SpriteView(scene: self.scene)
             .ignoresSafeArea()
     }
 }
